a9168d595238cebf06c0a53e5d07a8cc7d9ab475
nishikawa-nobuyuki <nishikawa.nobuyuki@unreact.jp>
2025-03-01 12:07:11 +0900

    recoil から jotai に修正

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,8 @@
 import { AppProps } from 'next/app';
-import { RecoilRoot } from 'recoil';
 
 import '@/styles/globals.css';
 
 function MyApp({ Component, pageProps: { ...pageProps } }: AppProps): JSX.Element {
-  return (
-    <RecoilRoot>
-      <Component {...pageProps} />
-    </RecoilRoot>
-  );
+  return <Component {...pageProps} />;
 }
 export default MyApp;
