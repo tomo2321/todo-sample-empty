6cd1d5e164e3c6f4374d7cea3188a2ae902d098b
tomo2 <tomo2321@gmail.com>
2025-07-03 23:14:43 +0900

    add: add chapter 9 of section 2

--- src/components/ThreeBoxContent.tsx
+++ src/components/ThreeBoxContent.tsx
@@ -1,34 +1,29 @@
-const ThreeBoxContent = (): JSX.Element => {
+type Props = {
+  title1: string;
+  content1: string;
+  title2: string;
+  content2: string;
+  title3: string;
+  content3: string;
+};
+
+const ThreeBoxContent = (props: Props): JSX.Element => {
+  const { title1, content1, title2, content2, title3, content3 } = props;
   return (
     <div className="grid grid-cols-3 gap-x-4">
       <div className="p-6 shadow-xl">
-        <h3 className="text-xl font-bold">This is a Title.</h3>
-        <p className="mt-4 text-base">
-          This is a description. This is a description. This is a description. This is a
-          description. This is a description. This is a description. This is a description. This is
-          a description. This is a description. This is a description. This is a description. This
-          is a description.
-        </p>
+        <h3 className="text-xl font-bold">{title1}</h3>
+        <p className="mt-4 text-base">{content1}</p>
       </div>
 
       <div className="p-6 shadow-xl">
-        <h3 className="text-xl font-bold">This is a Title.</h3>
-        <p className="mt-4 text-base">
-          This is a description. This is a description. This is a description. This is a
-          description. This is a description. This is a description. This is a description. This is
-          a description. This is a description. This is a description. This is a description. This
-          is a description.
-        </p>
+        <h3 className="text-xl font-bold">{title2}</h3>
+        <p className="mt-4 text-base">{content2}</p>
       </div>
 
       <div className="p-6 shadow-xl">
-        <h3 className="text-xl font-bold">This is a Title.</h3>
-        <p className="mt-4 text-base">
-          This is a description. This is a description. This is a description. This is a
-          description. This is a description. This is a description. This is a description. This is
-          a description. This is a description. This is a description. This is a description. This
-          is a description.
-        </p>
+        <h3 className="text-xl font-bold">{title3}</h3>
+        <p className="mt-4 text-base">{content3}</p>
       </div>
     </div>
   );
--- src/pages/section2.tsx
+++ src/pages/section2.tsx
@@ -3,9 +3,19 @@ import { NextPage } from 'next';
 import ThreeBoxContent from '@/components/ThreeBoxContent';
 
 const Page: NextPage = () => {
+  const title = 'This is a Title.';
+  const content =
+    'This is a description. This is a description. This is a description. This is a description. This is a description. This is a description. This is a description. This is a description.';
   return (
     <div className="mx-auto mt-10 max-w-4xl">
-      <ThreeBoxContent />
+      <ThreeBoxContent
+        title1={title}
+        content1={content}
+        title2={title}
+        content2={content}
+        title3={title}
+        content3={content}
+      />
     </div>
   );
 };
