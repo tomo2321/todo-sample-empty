fe71ffcd99643c0f7ece7a922825821a8cc5d4df
tomo2 <tomo2321@gmail.com>
2025-07-04 01:17:10 +0900

    add: add chapter 10 of section 3

---
+++ src/pages/section3.tsx
@@ -0,0 +1,25 @@
+import { NextPage } from 'next';
+import { useState } from 'react';
+
+import Button from '@/components/common/parts/Button';
+
+const Page: NextPage = () => {
+  const [count, setCount] = useState(0);
+
+  const onClickCountUp = () => {
+    setCount((prevState) => prevState + 1);
+  };
+
+  return (
+    <div className="mx-auto mt-10 max-w-4xl">
+      <div className="flex justify-center">
+        <div>
+          <h2 className="mb-4 text-center text-6xl">{count}</h2>
+          <Button onClick={onClickCountUp} label="Count Up" variant="primary"></Button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Page;
